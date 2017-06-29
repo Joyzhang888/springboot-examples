a6e518dde5c87326fd9ded5ce12caa5947a32e19
hong <1043518275@qq.com>
2017-06-29 18:14:15 +0800

    redis 使用 annotation 方式作为缓存.

--- chapter11-caching-redis/src/main/java/com/hong/config/RedisCacheConfiguration.java
+++ chapter11-caching-redis/src/main/java/com/hong/config/RedisCacheConfiguration.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
 
 
 /**
@@ -26,8 +29,31 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @EnableCaching
 public class RedisCacheConfiguration {
 
+    /**
+     * 自定义生key的方式,可以不用配置使用默认的.
+     * @return
+     */
+    @Bean
+    public KeyGenerator customKeyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object o, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(o.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
 
-
+    /**
+     * 配置缓存管理器为RedisCacheManager.
+     * @param redisTemplate
+     * @return
+     */
     @Bean
     public CacheManager cacheManager(RedisTemplate redisTemplate) {
         RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate);
@@ -36,6 +62,12 @@ public class RedisCacheConfiguration {
         return redisCacheManager;
     }
 
+
+    /**
+     * 配置RedisTemplate 信息.
+     * @param factory
+     * @return
+     */
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
@@ -44,6 +76,7 @@ public class RedisCacheConfiguration {
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
+        //使用Jackson库将对象序列化为JSON字符串。
         template.setValueSerializer(jackson2JsonRedisSerializer);
         template.afterPropertiesSet();
         return template;
