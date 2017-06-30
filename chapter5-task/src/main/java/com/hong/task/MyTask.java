da169e80cbc2ed262b25182ef914013efc88007c
hong <1043518275@qq.com>
2017-06-30 16:08:27 +0800

    spring boot 系列计划说明.

--- chapter5-task/src/main/java/com/hong/task/MyTask.java
+++ chapter5-task/src/main/java/com/hong/task/MyTask.java
@@ -14,6 +14,20 @@ import java.util.Date;
 @EnableScheduling
 public class MyTask {
 
+    /**
+     * @Scheduled 详细参数说明：
+     *
+     * 1.corn    指定：秒，分钟，小时，日期，月份，星期，年（可选）
+     * 2.fixedDelay   从上一个任务完成到下一个任务开始的间隔，单位是毫秒
+     * 3.fixedDelayString  同上,只是字符串类型.
+     * 4.fixedRate    从上一个任务开始到下一个任务开始的间隔，单位是毫秒
+     * 5.fixedRateString  同上,只是字符串类型.
+     * 6.initialDelay   任务第一次执行前需要延迟的毫秒数
+     * 7.initialDelayString   同上,只是字符串类型.
+     * 8.zone    指定时区
+     *
+     */
+
 
     /**
      * 这个方法每10秒打印一次.
@@ -21,8 +35,25 @@ public class MyTask {
      */
     @Scheduled(cron = "0/10 * * * * *")
     public void test(){
-        System.out.println("mytask......"+new Date());
+        System.out.println("test......"+new Date().getTime());
     }
+
+
+    /**
+     * 每隔6秒执行.
+     */
+    @Scheduled(fixedDelay = 6000)
+    public void test2(){
+        System.out.println("test2..."+new Date().getTime());
+    }
+
+//    @Scheduled(fixedDelayString = "5000")
+//    public void test3(){
+//        System.out.println("test3..."+new Date().getTime());
+//    }
+
+
+
 
 
 }
